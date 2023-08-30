2c72e5fc4fb0bbd077cbd9899f8573ef1be76a52
kalpizard <kalpizard@fwdcostarica.com>
2023-08-30 15:03:30 -0600

    boton fwd

--- src/Components/Foot.js
+++ src/Components/Foot.js
@@ -1,31 +1,35 @@
 import React from "react";
 import "../styles/Foot.css";
 
+import fwd from "../FWD.jpeg";
 
 function Foot() {
   return (
     <div className="pie">
-      
-      <div className="dedo_1">
+     
 
-
-      </div>
-
-      <div className="dedo_2">
-
-        
-      </div>
-
-      <div className="dedo_3">
-
-        
-      </div>
+    
+<div className="dedo_1">
+    <button type="button">
+        <img
+            src={fwd} // Utilizando la variable 'fwd' que importaste
+            alt="Descripción de la imagen"
+            style={{ width: "100%", height: "100%" }}
+        />
+    </button>
+</div>
 
 
 
 
 
 
+
+
+
+      <div className="dedo_2"></div>
+
+      <div className="dedo_3"></div>
     </div>
   );
 }
