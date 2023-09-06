1dce7cb866f04a15b54dbbdf7e4bb41294e4b6ed
kalpizard <kalpizard@fwdcostarica.com>
2023-09-06 09:48:22 -0600

    miercoles 9am

--- src/Components/CuerpoPokedex.js
+++ src/Components/CuerpoPokedex.js
@@ -1,61 +1,66 @@
-import React, { useEffect, useState } from 'react'
-import { getPokemonList } from '../Api'; 
+import React, { useEffect, useState } from "react";
+import { getPokemonList } from "../Api";
+import "../styles/cuerpoPokedex.css";
 
 
 
 function Pokedex33() {
-
   const [Pokemons, setPokemons] = useState([]);
   const [isLoading, setisLoading] = useState(false);
-  const [newFavorite , setNewFavorite] = useState ('');
-  const [loves, setLoves] = useState (0);
+  const [newFavorite, setNewFavorite] = useState("");
+  const [loves, setLoves] = useState();
 
-  const handleLoves = () => { 
-
-  setLoves(loves +1);
+  const handleLoves = (e) => {
+    e.target.getElementsByTagName("span")[0].textContent = 1;
   };
 
-    useEffect(() => {
-      const fetchPokemon = async () => {
-        try {
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      try {
+        const url = "https://pokeapi.co/api/v2/pokemon?Limit=20";
+        const fetchedPokemons = [];
+        const response = await getPokemonList(url);
+        const data = response.array;
 
-         const url = "https://pokeapi.co/api/v2/pokemon?Limit=20"
-         const fetchedPokemons = [];
-         const response= await getPokemonList(url);
-         const data=response.array
+        setPokemons(data);
+      } catch (error) {
+        console.error("Error capturando Pokemon data", error);
+      }
+      setisLoading(true);
+    };
+    fetchPokemon();
+  }, []);
 
-         setPokemons(data);
+  return (
+    <div className="contenedor">
+      {isLoading ? (
+        Pokemons.map((item, index) => {
+          return (
+            <div className="book" key={index}>
 
-        } catch (error) {
-          console.error("Error capturando Pokemon data", error);
-        }
-        setisLoading(true);
-      };
-      fetchPokemon();
+              <img src={item.image} />
+                 <div  className="cover">
+                           
 
-    },[]);
- 
-    return (
+              <p className="name">{item.name}</p>
+              <p className="name">{item.id}</p>
+                 </div>
 
-      <div className="contenedor">
-          {isLoading ? (
-          Pokemons.map((item) => {
 
-    return (
-        <div className='card' key={item}>
-        <img src={item.image}/>
-        <p className='name'>{item.name}</p>
-        <p className='name'>{item.id}</p>
-        <button onClick={handleLoves}>{loves} {loves ===1? 'Love♥':'Loves♥'}</button>
-  </div>
-        );
-      })
-    ) : (
-      <h2 className='loading'>Esta Cargando...</h2>
-    )}
+              <button onClick={handleLoves}>
+                <span ></span>
+                {loves} {loves === 1 ? "Love♥" : "Loves♥"}
+              
+              </button>
+            </div>
+          );
+          console.log(handleLoves);
+        })
+      ) : (
+        <h2 className="loading">Esta Cargando...</h2>
+      )}
     </div>
- 
-  )
+  );
 }
 
 export default Pokedex33;
